60e60b1c19cfb7bb8a820ea2ee81ef2bd4262e79
unknown <pinedavictor095@gmail.com>
2020-11-07 09:22:21 -0800

    Threshold

--- src/Components/Threshold.jsx
+++ src/Components/Threshold.jsx
@@ -4,34 +4,30 @@ import Button from "@material-ui/core/Button";
 
 export const Threshold = (props) => {
   const handleClick = () => {
-    console.log("CLIcking");
     let mat = window.cv.imread(props.img.current);
-    console.log("My mat::::", mat);
-
-    let dst = new window.cv.Mat();
     let gray = new window.cv.Mat();
 
-    // gray and threshold image
+    // Gray threshold image
     window.cv.cvtColor(mat, gray, window.cv.COLOR_RGBA2GRAY, 0);
+    // Apply threshold
     window.cv.threshold(
       gray,
       gray,
-      0,
+      100,
       255,
       window.cv.THRESH_BINARY_INV + window.cv.THRESH_OTSU
     );
 
     window.cv.imshow("canvasOutput", gray);
     mat.delete();
-    dst.delete();
     gray.delete();
   };
   return (
     <>
       <Container>
         <h1>HI :) </h1>
-        <Button onClick={handleClick} style={{ backgroundColor: "blue" }}>
-          <p>Turn to Vector</p>
+        <Button onClick={handleClick} style={{ backgroundColor: "white" }}>
+          <p>Image Trace</p>
         </Button>
       </Container>
     </>
